3e4129839b23c5ed6a0b5e1ddab3d1c965d26747
Robert Hardy <rwhj77@hotmail.com>
2021-01-17 18:14:28 -0600

    commit tree e63a3eb34754827cc1c4338135bb1ca33da1ab4c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(int)
+    while int > 0
+     puts "#{int} SECOND(S)!"
+     int -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
 
-def countdown
-end
+  
+  def countdown_with_sleep(int)
+    while int > 0
+     puts "#{int} SECOND(S)!"
+     int -= 1
+     sleep 1
+    end
+  end
